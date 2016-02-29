5fa155bd87a786e99a6665cbe2b6cd80e9a052df
mprescher <mprescher@users.noreply.github.com>
2016-02-29 10:23:16 -0800

    Update sequences.js

--- sequences.js
+++ sequences.js
@@ -1,6 +1,6 @@
 // Dimensions of sunburst.
-var width = 750;
-var height = 600;
+var width = 250;
+var height = 200;
 var radius = Math.min(width, height) / 2;
 
 // Breadcrumb dimensions: width, height, spacing, width of tip/tail.

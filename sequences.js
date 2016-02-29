52dfe6dbf9582e490d9523028ed8c029db0ed2eb
mprescher <mprescher@users.noreply.github.com>
2016-02-29 10:30:14 -0800

    Update sequences.js

--- sequences.js
+++ sequences.js
@@ -10,7 +10,7 @@ var b = {
 
 // Mapping of step names to colors.
 var colors = {
-  "Supply": "#5687d1",
+  "home": "#5687d1",
   "product": "#7b615c",
   "search": "#de783b",
   "account": "#6ab975",
